f8b0177f54329a5be0ad977983238034380f3d33
이준희 <juni416@naver.com>
2023-09-28 18:05:13 +0900

    feature: 스터디생성, 앱링크 생성시 studyCode를 넣게하기

--- domain/src/main/kotlin/com/cheongseolmo/domain/study/service/StudyCommandProcessor.kt
+++ domain/src/main/kotlin/com/cheongseolmo/domain/study/service/StudyCommandProcessor.kt
@@ -4,12 +4,10 @@ import com.cheongseolmo.domain.account.repository.AccountRepository
 import com.cheongseolmo.domain.study.contract.command.CreateStudyCodeCommand
 import com.cheongseolmo.domain.study.contract.command.StudyCodeRead
 import com.cheongseolmo.domain.study.entity.Study
-import com.cheongseolmo.domain.study.entity.StudyCode
 import com.cheongseolmo.domain.study.exception.NotFoundStudyException
 import com.cheongseolmo.domain.study.repository.StudyRepository
 import com.cheongseolmo.domain.study.usecase.StudyCommandUseCase
 import com.cheongseolmo.domain.study.usecase.StudyInviteUseCase
-import java.util.*
 
 open class StudyCommandProcessor(
     val studyRepository: StudyRepository,
@@ -28,9 +26,9 @@ open class StudyCommandProcessor(
         return StudyCodeRead.of(studyCode)
     }
 
-    override fun createAppLink(studyKey: UUID, email: String, appLink: String): String {
+    override fun createAppLink(studyCode: String, email: String, appLink: String): String {
         // TODO("앱링크결정이 안돼서 임시로 처리함.")
         val account = accountRepository.findByEmail(email) ?: throw Exception("계정이 없습니다")
-        return "${appLink}://join-waiting-room?accountKey=${account.key}&studyKey=${studyKey}"
+        return "${appLink}://join-waiting-room?accountKey=${account.key}&studyCode=${studyCode}"
     }
 }
